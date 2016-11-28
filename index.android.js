da4561c3858875cbeeb39a6ebf4f1526a6264b54
fmblzf <fmblzf@example.com>
2016-11-28 18:11:20 +0800

    hello world创建

--- index.android.js
+++ index.android.js
@@ -9,45 +9,45 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
+  Image,
   View
 } from 'react-native';
 
-export default class ReactProject extends Component {
-  render() {
+
+class ReactProjects extends Component{
+
+    render(){
+      let pic = {
+          uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+      };
+      return (
+        <Image source={pic} style={{width:193,height:110}} />
+      );
+    }
+
+}
+
+class Greeting extends Component{
+  render(){
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
+        <Text>
+          Hello {this.props.name}!
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+    );
+  }
+}
+
+class ReactProject extends Component{
+  render(){
+    return (
+      <View style={{alignItems:'center'}}>
+      <Greeting name='Rexxar' />
+      <Greeting name='Jaina' />
+      <Greeting name='Valeera' />
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('ReactProject', () => ReactProject);
