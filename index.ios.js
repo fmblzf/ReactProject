98ad867969ca6a0589676464ad2eada0718aa7a2
fmblzf <fmblzf@example.com>
2016-11-29 18:30:12 +0800

    基本组件和语法练习，ScrollView和ListView组件使用

--- index.ios.js
+++ index.ios.js
@@ -37,6 +37,24 @@ class Greeting extends Component{
   }
 }
 
+class Blink extends Component{
+  constructor(props){
+    super(props);
+    this.state = {showText:true};
+    //设置定时器每1秒做一次取反
+    setInterval(()=>{
+      this.setState({showText:!this.state.showText});
+    },1000)
+  }
+  render(){
+    let display = this.state.showText?this.props.text:'';
+    return (
+      <Text>{display}</Text>
+    );
+
+  }
+}
+
 class ReactProject extends Component{
   render(){
     return (
@@ -44,6 +62,10 @@ class ReactProject extends Component{
       <Greeting name='Rexxar' />
       <Greeting name='Jaina' />
       <Greeting name='Valeera' />
+      <Blink text='I love to blink ' />
+      <Blink text='Yes blinking is so great ' />
+      <Blink text='Why did they ever take this out of HTML ' />
+      <Blink text='Look at me look at me look at me ' />
       </View>
     );
   }
