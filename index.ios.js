22826b25f7c4ce3b884b430bf1f0e3e58ad47586
fmblzf <fmblzf@example.com>
2016-11-30 16:43:26 +0800

    同步ios代码

--- index.ios.js
+++ index.ios.js
@@ -10,7 +10,10 @@ import {
   StyleSheet,
   Text,
   Image,
-  View
+  View,
+  TextInput,
+  ScrollView,
+  ListView
 } from 'react-native';
 
 
@@ -42,9 +45,9 @@ class Blink extends Component{
     super(props);
     this.state = {showText:true};
     //设置定时器每1秒做一次取反
-    setInterval(()=>{
-      this.setState({showText:!this.state.showText});
-    },1000)
+    // setInterval(()=>{
+    //   this.setState({showText:this.state.showText});
+    // },1000)
   }
   render(){
     let display = this.state.showText?this.props.text:'';
@@ -55,21 +58,144 @@ class Blink extends Component{
   }
 }
 
-class ReactProject extends Component{
+class FixedDimensionsBasics extends Component{
   render(){
     return (
-      <View style={{alignItems:'center'}}>
-      <Greeting name='Rexxar' />
-      <Greeting name='Jaina' />
-      <Greeting name='Valeera' />
-      <Blink text='I love to blink ' />
-      <Blink text='Yes blinking is so great ' />
-      <Blink text='Why did they ever take this out of HTML ' />
-      <Blink text='Look at me look at me look at me ' />
+      <View>
+        <View style={{width:50,height:50,backgroundColor:'powderblue'}} />
+        <View style={{width:100,height:100,backgroundColor:'skyblue'}} />
+        <View style={{width:150,height:150,backgroundColor:'steelblue'}} />
       </View>
     );
   }
 }
+class FixedBasics extends Component{
+  render(){
+    return (
+      <View style={{flex:1}}>
+        <View style={{flex:1,backgroundColor:'powderblue'}} />
+        <View style={{flex:2,backgroundColor:'skyblue'}} />
+        <View style={{flex:3,backgroundColor:'steelblue'}} />
+      </View>
+    );
+  }
+}
+class FixedDirectionBasicesLayout extends Component{
+  render(){
+    return (
+      <View style={{flex:1,flexDirection:'column',justifyContent:'center',alignItems:'stretch',backgroundColor:'red'}}>
+        <View style={{height:50,backgroundColor:'powderblue'}} />
+        <View style={{height:50,backgroundColor:'skyblue'}} />
+        <View style={{height:50,backgroundColor:'steelblue'}} />
+      </View>
+    );
+  }
+}
+class PizzTranslator extends Component{
+  constructor(props){
+    super(props);
+    this.state = {text:''};
+  }
+  render(){
+    return (
+      <View style={{padding:10}}>
+        <TextInput style={{height:40}} placeholder="Type here to translate!" 
+          onChangeText={(text)=>this.setState({text})}
+        />
+        <Text style={{padding:10,fontSize:42}} >
+          {this.state.text.split(' ').map((word)=>word&&'🍕').join(' ')}
+        </Text>
+      </View>
+    );
+  }
+}
+
+class IScrolledDownAndWhatHappenedNextShockedMe extends Component{
+  render(){
+    return (
+      <ScrollView>
+        <Text style={{fontSize:96}}>Scroll me plz</Text>
+        <Image source={require('./img/fav.png')} />
+        <Image source={require('./img/fav.png')} />
+        <Image source={require('./img/fav.png')} />
+        <Image source={require('./img/fav.png')} />
+        <Image source={require('./img/fav.png')} />
+        <Text style={{fontSize:96}}>Scroll me plz</Text>
+        <Image source={require('./img/fav.png')} />
+        <Image source={require('./img/fav.png')} />
+        <Image source={require('./img/fav.png')} />
+        <Image source={require('./img/fav.png')} />
+        <Image source={require('./img/fav.png')} />
+        <Text style={{fontSize:96}}>if you like </Text>
+        <Image source={require('./img/fav.png')} />
+        <Image source={require('./img/fav.png')} />
+        <Image source={require('./img/fav.png')} />
+        <Image source={require('./img/fav.png')} />
+        <Image source={require('./img/fav.png')} />
+        <Text style={{fontSize:96}}>Scrolling down</Text>
+        <Image source={require('./img/fav.png')} />
+        <Image source={require('./img/fav.png')} />
+        <Image source={require('./img/fav.png')} />
+        <Image source={require('./img/fav.png')} />
+        <Image source={require('./img/fav.png')} />
+        <Text style={{fontSize:96}}>What's the best</Text>
+        <Image source={require('./img/fav.png')} />
+        <Image source={require('./img/fav.png')} />
+        <Image source={require('./img/fav.png')} />
+        <Image source={require('./img/fav.png')} />
+        <Image source={require('./img/fav.png')} />
+        <Text style={{fontSize:96}}>Framework around?</Text>
+        <Image source={require('./img/fav.png')} />
+        <Image source={require('./img/fav.png')} />
+        <Image source={require('./img/fav.png')} />
+        <Image source={require('./img/fav.png')} />
+        <Image source={require('./img/fav.png')} />
+        <Text style={{fontSize:96}}>React Native</Text>
+      </ScrollView>
+    );
+  }
+}
+class ListViewBasics extends Component{
+  constructor(props){
+    super(props);
+    const ds = new ListView.DataSource({rowHasChanged:(r1,r2)=>r1!==r2});
+    this.state = {
+      dataSource:ds.cloneWithRows([
+        'John','Joel','Aslice','Slice','Python','Dvide','Sfsf','Mkkiuhai'
+      ])
+    };
+  }
+  render(){
+    return (
+      <View style={{flex:1,paddingTop:22}}>
+        <ListView 
+          dataSource={this.state.dataSource}
+          renderRow={(rowData)=><Text style={{height:100}}>{rowData}</Text>}
+         />
+      </View>
+    );
+  }
+}
+class ReactProject extends Component{
+  render(){
+    return (
+      <ListViewBasics />
+
+      
+    );
+  }
+}
+
+const style = StyleSheet.create({
+  bigblue:{
+    color:'blue',
+    fontWeight:'bold',
+    fontSize:30,
+  },
+  red:{
+    color:'red',
+  },
+});
 
 
 AppRegistry.registerComponent('ReactProject', () => ReactProject);
