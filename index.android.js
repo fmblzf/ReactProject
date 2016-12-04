3dfba541bc231ec5783c2f98e37949403dd03523
fmblzf <277421352@qq.com>
2016-12-04 21:40:48 +0800

    动画使用

--- index.android.js
+++ index.android.js
@@ -4,53 +4,83 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
 import {
   AppRegistry,
-  Navigator
+  Navigator,
+  Text,
+  View,
 } from 'react-native';
 
 
 import Scene from './component/scene';
+import Playground from './component/animate'; 
 
-class ReactProject extends Component{
+class ReactProject extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      topics: []
+
+    };
+  }
   fetchData = () => {
     return fetch('http://bbs.reactnative.cn/api/category/3')
       .then((response) => response.json())
       .then((responseJson) => {
         console.log(responseJson)
+        this.setState({
+          topics: responseJson.topics
+        })
       })
       .catch((error) => {
         console.error(error);
       });
   };
-  render(){
-    return (
+  render() {
+    return (      
       <Navigator 
-        initialRoute={{title:'My Initial Scene',index:0}}
-        renderScene={(route,navigator)=>
-          <Scene 
-            title={route.title} 
-            //Function to call when a new scene should be displayed
-            onForward={()=>{
-              const nextIndex = route.index+1;
-              navigator.push({
-                title:'Scene'+nextIndex,
-                index:nextIndex
-              });
-            }}
-            //Function to call to go back the previous scene 
-            onBack={()=>{
-              if (route.index > 0) {
-                navigator.pop();
+        initialRoute = {{
+          title: 'My Initial Scene',
+          index: 0
+        }}
+        renderScene = {(route, navigator) =>
+          <View style={{flex:1}}>
+            <Scene
+              title = {
+                route.title
               }
-            }}
-            //Funtion to fetch data
-            onFetch={this.fetchData}
-          />
-          
-        }
-      />      
+              //Function to call when a new scene should be displayed
+              onForward = {() => {
+                const nextIndex = route.index + 1;
+                navigator.push({
+                  title: 'Scene' + nextIndex,
+                  index: nextIndex
+                });
+              }}
+              //Function to call to go back the previous scene 
+              onBack = {() => {
+                if (route.index > 0) {
+                  navigator.pop();
+                }
+              }}
+              //Funtion to fetch data
+              onFetch = {
+                this.fetchData
+              }
+            />
+            <View accessible={true}>
+              <Text>text one</Text>
+              <Text>text two</Text>
+            </View>
+
+            <Playground />
+        </View>
+      }
+      /> 
     );
   }
 }
