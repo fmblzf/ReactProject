f789dde66812fe3963748569c98e238c9dbb40b9
fmblzf <fmblzf@example.com>
2016-12-21 14:41:54 +0800

    修改模块调用的问题

--- android/app/src/main/java/com/reactproject/MainApplication.java
+++ android/app/src/main/java/com/reactproject/MainApplication.java
@@ -3,9 +3,6 @@ package com.reactproject;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.rnfs.RNFSPackage;
-import com.remobile.zip.RCTZipPackage;
-import com.remobile.filetransfer.RCTFileTransferPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -30,21 +27,18 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new SQLitePluginPackage(),//注册Sqlite插件
-          new MainReactPackage(),
-            new RNFSPackage(),
-            new RCTZipPackage(),
-            new RCTFileTransferPackage()
+          new MainReactPackage()
       );
     }
 
-    @Nullable
-    @Override
-    protected String getJSBundleFile() {
-      //实现自定义jsbundle的路径设置,热更新时，可以做出判断加载
-      String jsbundlepath = getFilesDir().getAbsolutePath()+"/index.android.bundle";
-      File file = new File(jsbundlepath);
-      return file!=null?jsbundlepath:null;
-    }
+    // @Nullable
+    // @Override
+    // protected String getJSBundleFile() {
+    //   //实现自定义jsbundle的路径设置,热更新时，可以做出判断加载
+    //   String jsbundlepath = getFilesDir().getAbsolutePath()+"/index.android.bundle";
+    //   File file = new File(jsbundlepath);
+    //   return file!=null?jsbundlepath:null;
+    // }
   };
 
   @Override
